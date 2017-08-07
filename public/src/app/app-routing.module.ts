a55ac9b0214987d9c0b89c86c49e72a0e378c8bd
Steve Kim <suykim21@Steves-MacBook.local>
2017-08-07 12:57:28 -0700

    pulling jakes navbar and stripe

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EventsComponent } from './events/events.component';
+import { AboutComponent } from './about/about.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  {path: 'events', component: EventsComponent},
+  {path: 'about', component: AboutComponent},
 ];
 
 @NgModule({
