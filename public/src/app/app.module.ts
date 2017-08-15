b4fc85b3084dc09dc45ec73717d219439cc32c73
Jake Kiernan <kiernan.jake@gmail.com>
2017-08-15 13:53:57 -0500

    added navbar modal

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
